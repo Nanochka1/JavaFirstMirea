abe6c8e5d4b9c1ec044a908f1be4cdceea859bd7
Nanochka1 <Anastasia2303>
2021-09-30 23:10:56 +0300

    Task 9

---
+++ src/ru/mirea/task9/Test.java
@@ -0,0 +1,10 @@
+package ru.mirea.task9;
+
+import javax.swing.*;
+
+public class Test {
+    public static void main(String[] args) {
+        Match app = new Match();
+        app.setVisible(true);
+    }
+}
